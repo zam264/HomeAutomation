3de3d7333fdef548da43f3d879ef4175b838ae47
Steven Zamborsky <zam264@gmail.com>
2015-02-17 17:40:32 -0500

    Basic framework and structure created
    There is now a menu screen that lets the user go to a 'Controls' screen
    and a 'Schedule' screen.  These screens are bare except for title text
    and back buttons.

---
+++ controls.lua
@@ -0,0 +1,109 @@
+local composer = require( "composer" )
+local scene = composer.newScene()
+local widget = require "widget"		-- include Corona's "widget" library
+---------------------------------------------------------------------------------
+-- All code outside of the listener functions will only be executed ONCE
+-- unless "composer.removeScene()" is called.
+---------------------------------------------------------------------------------
+display.setDefault( "background", .25 )
+-- local forward references should go here
+local titleText1, backBtn
+local contentWidth = display.contentWidth
+local contentHeight = display.contentHeight
+
+
+local function onBackBtn()
+	composer.gotoScene( "menu", {effect="fromLeft", time=1000})
+
+	return true	-- indicates successful touch
+end
+
+---------------------------------------------------------------------------------
+
+-- "scene:create()"
+function scene:create( event )
+    sceneGroup = self.view	
+	
+	titleText1 = display.newText( "Controls", contentWidth * .5, contentHeight*.1, "fonts/Rufscript010" ,contentHeight * .065)
+	sceneGroup:insert(titleText1)
+   
+   -- Initialize the scene here.
+	backBtn = widget.newButton{
+		label="Back",
+		fontSize = contentWidth * .05,
+		labelColor = { default={255}, over={128} },
+		defaultFile="imgs/button.png",
+		overFile="imgs/button-over.png",
+		width=contentWidth * .50, height=contentHeight * .1,
+		onRelease = onBackBtn
+	}
+	backBtn.anchorX = .5
+	backBtn.anchorY = .5
+	backBtn.x = contentWidth * .50
+	backBtn.y = contentHeight * .85
+	sceneGroup:insert(backBtn)	
+	
+   -- Example: add display objects to "sceneGroup", add touch listeners, etc.
+end
+
+-- "scene:show()"
+function scene:show( event )
+
+   local sceneGroup = self.view
+   local phase = event.phase
+
+   if ( phase == "will" ) then
+		titleText1.isVisible = true
+		backBtn.isVisible = true
+      -- Called when the scene is still off screen (but is about to come on screen).
+   elseif ( phase == "did" ) then
+      -- Called when the scene is now on screen.
+      -- Insert code here to make the scene come alive.
+      -- Example: start timers, begin animation, play audio, etc.
+   end
+end
+
+-- "scene:hide()"
+function scene:hide( event )
+
+   local sceneGroup = self.view
+   local phase = event.phase
+
+   if ( phase == "will" ) then
+   		titleText1.isVisible = false
+		backBtn.isVisible = false
+      -- Called when the scene is on screen (but is about to go off screen).
+      -- Insert code here to "pause" the scene.
+      -- Example: stop timers, stop animation, stop audio, etc.
+   elseif ( phase == "did" ) then
+      -- Called immediately after scene goes off screen.
+   end
+end
+
+-- "scene:destroy()"
+function scene:destroy( event )
+
+   local sceneGroup = self.view
+
+	titleText1:removeSelf()
+	titleText1 = nil
+	backBtn:removeSelf()
+	backBtn = nil
+
+	
+   -- Called prior to the removal of scene's view ("sceneGroup").
+   -- Insert code here to clean up the scene.
+   -- Example: remove display objects, save state, etc.
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+---------------------------------------------------------------------------------
+
+return scene
---
+++ main.lua
@@ -0,0 +1,35 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- include the Corona "storyboard" module
+local composer = require "composer"
+
+-- load menu screen
+local rect
+holding = {}
+inUse = {}
+
+ 
+composer.gotoScene( "menu" )
+
+--Codes the keys for back button functionality 
+local function onKeyEvent(event)
+	local phase = event.phase
+    local keyName = event.keyName 
+    
+	if ( "back" == keyName and phase == "up" ) then
+		--composer.gotoScene( storyboard.returnTo, "fade", 250 )
+		return true	-- indicates successful touch
+		
+	else 
+		return false ; 
+	end
+end
+
+Runtime:addEventListener( "key", onKeyEvent )
